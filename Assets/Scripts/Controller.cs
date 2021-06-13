24851740eb603ff6e5989b5e1c8ff8f8df307604
Luca Barre <xlucarix@gmail.com>
2021-06-13 17:39:33 +0200

    fixed rotation-problem

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -15,11 +15,20 @@ public class Controller : MonoBehaviour {
 	}
 
 	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
 
-		RaycastHit
+		if(Physics.Raycast(ray, out hit))
+        {
+			Vector3 mp = new Vector3(hit.point.x, 1.5f, hit.point.z);
+			transform.LookAt(mp);
+        }
+		/*
 		Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
 		transform.LookAt (mousePos + Vector3.up * transform.position.y);
+		*/
 		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
+		
 	}
 
 	void FixedUpdate() {
